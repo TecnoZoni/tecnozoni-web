bc8e88f7f223698b703ba6427faa0bb501ab5fcb
TecnoZoni <agustinminzoni@gmail.com>
2023-04-07 06:35:54 -0300

    portfolio complete

---
+++ script.js
@@ -0,0 +1,85 @@
+// responsive navbar
+let menuIcon = document.querySelector('#menu-icon');
+let nevbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nevbar.classList.toggle('active');
+}
+
+
+
+// activar links de las sections
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar
+    const header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // quitar el boton del navbar cuando clickeamos un link
+    menuIcon.classList.remove('bx-x');
+    nevbar.classList.remove('active');
+
+};
+
+
+// scroll reveal
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img , .services-container , .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1 , .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p , .about-content', { origin: 'right' });
+
+// typed js 
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desarrollador Frontend', 'Diseñador Frontend'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+// enviar email
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Enviando...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_3wxu4mr';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Enviar Mensaje';
+                alert('Mensaje enviado!');
+            }, (err) => {
+                btn.value = 'Enviar Mensaje';
+                alert(JSON.stringify(err));
+            });
+    });
